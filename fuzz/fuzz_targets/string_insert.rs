e69f90a5f87dd91207778f0f0d45a3d16b708b40
Thomas Krause <thomas.krause@hu-berlin.de>
2022-02-09 13:37:43 +0100

    Also check len() and get() function in fuzzing tests

--- fuzz/fuzz_targets/string_insert.rs
+++ fuzz/fuzz_targets/string_insert.rs
@@ -6,17 +6,28 @@ use transient_btree_index::{BtreeConfig, BtreeIndex, Error};
 fuzz_target!(|data: (Vec<(String, String)>, u8)| {
     let order = data.1.max(2);
     let mut m = BTreeMap::default();
-    let mut t = BtreeIndex::with_capacity(BtreeConfig::default().with_order(order), 1024).unwrap();
+    let mut fixture =
+        BtreeIndex::with_capacity(BtreeConfig::default().with_order(order), 1024).unwrap();
 
     for (key, value) in data.0 {
         m.insert(key.clone(), value.clone());
-        t.insert(key, value).unwrap();
+        fixture.insert(key, value).unwrap();
     }
 
-    // Check that the maps are equal
-    let m: Vec<_> = m.into_iter().collect();
-    let t: Result<Vec<_>, Error> = t.range(..).unwrap().collect();
-    let t = t.unwrap();
+    // Check len() function
+    assert_eq!(m.len(), fixture.len());
 
-    assert_eq!(m, t);
+    // get query for each entry
+    for (k, v1) in m.iter() {
+        assert!(fixture.contains_key(k).unwrap());
+        let v2 = fixture.get(k).unwrap();
+        assert_eq!(Some(v1.clone()), v2);
+    }
+
+    // Check that the maps are equal with differnt range queries
+    let m: Vec<_> = m.into_iter().collect();
+    let fixture_result: Result<Vec<_>, Error> = fixture.range(..).unwrap().collect();
+    let fixture_result = fixture_result.unwrap();
+
+    assert_eq!(m, fixture_result);
 });
