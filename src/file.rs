1a464b2e53a3fa51be51f163dab3d2e7b57c6946
Thomas Krause <thomaskrause@posteo.de>
2022-02-13 11:56:39 +0100

    Begin to implement a mempry mapped file holding only BTree nodes and references to the key, but which does not be to deserialized to be read.

--- src/file.rs
+++ src/file.rs
@@ -6,6 +6,7 @@ use std::{
 };
 
 use crate::{error::Result, PAGE_SIZE};
+use binary_layout::{prelude::*, FieldView};
 use bincode::Options;
 use linked_hash_map::LinkedHashMap;
 use memmap2::{MmapMut, MmapOptions};
@@ -19,6 +20,129 @@ pub fn page_aligned_capacity(capacity: usize) -> usize {
     }
     // Make sure there is enough space for the block header
     (num_full_pages * PAGE_SIZE) - BlockHeader::size()
+}
+
+// Defines a single BTree node with references to the actual values in a tuple file
+// This node can up to 1361 keys and 1362 child node references.
+define_layout!(node, LittleEndian, {
+    id: u64,
+    num_keys: u16,
+    is_leaf: u8,
+    keys: [u8; 10888],
+    payloads: [u8; 10888],
+    child_nodes: [u8; 10896],
+});
+
+const NODE_BLOCK_SIZE: usize = 4095;
+const NODE_BLOCK_ALIGNED_SIZE: usize = PAGE_SIZE;
+
+pub fn get_key(field: &FieldView<&[u8], node::keys>, i: u8) -> Result<u64> {
+    let offset: usize = (i as usize) * 8;
+    let result: u64 = u64::from_le_bytes(field.data()[offset..(offset + 8)].try_into()?);
+    Ok(result)
+}
+
+pub fn set_key(field: &mut FieldView<&mut [u8], node::keys>, i: u8, value: usize) {
+    let offset: usize = (i as usize) * 8;
+    let value = value.to_le_bytes();
+    field.data_mut()[offset..].copy_from_slice(&value);
+}
+
+pub fn get_payload(field: &FieldView<&[u8], node::payloads>, i: u8) -> Result<u64> {
+    let offset: usize = (i as usize) * 8;
+    let result: u64 = u64::from_le_bytes(field.data()[offset..(offset + 8)].try_into()?);
+    Ok(result)
+}
+
+pub fn set_payload(field: &mut FieldView<&mut [u8], node::payloads>, i: u8, value: usize) {
+    let offset: usize = (i as usize) * 8;
+    let value = value.to_le_bytes();
+    field.data_mut()[offset..].copy_from_slice(&value);
+}
+
+pub fn get_child_node(field: &FieldView<&[u8], node::child_nodes>, i: u8) -> Result<u64> {
+    let offset: usize = (i as usize) * 8;
+    let result: u64 = u64::from_le_bytes(field.data()[offset..(offset + 8)].try_into()?);
+    Ok(result)
+}
+
+pub fn set_child_node(field: &mut FieldView<&mut [u8], node::child_nodes>, i: u8, value: usize) {
+    let offset: usize = (i as usize) * 8;
+    let value = value.to_le_bytes();
+    field.data_mut()[offset..].copy_from_slice(&value);
+}
+
+pub struct NodeFile {
+    free_space_offset: usize,
+    mmap: MmapMut,
+}
+
+impl NodeFile {
+    pub fn with_capacity(capacity: usize) -> Result<NodeFile> {
+        // Create an anonymous memory mapped file with the capacity as size
+        let capacity = capacity.max(1);
+        let mmap = MmapOptions::new().stack().len(capacity).map_anon()?;
+
+        Ok(NodeFile {
+            mmap,
+            free_space_offset: 0,
+        })
+    }
+
+    pub fn get(&self, node_id: u64) -> Result<node::View<&[u8]>> {
+        let node_id: usize = node_id.try_into()?;
+        let offset: usize = NODE_BLOCK_ALIGNED_SIZE * node_id;
+        let view = node::View::new(&self.mmap[offset..(offset + NODE_BLOCK_SIZE)]);
+        Ok(view)
+    }
+
+    pub fn get_mut(&mut self, node_id: u64) -> Result<node::View<&mut [u8]>> {
+        let node_id: usize = node_id.try_into()?;
+        let offset: usize = NODE_BLOCK_ALIGNED_SIZE * node_id;
+        let view = node::View::new(&mut self.mmap[offset..(offset + NODE_BLOCK_SIZE)]);
+        Ok(view)
+    }
+
+    /// Allocate a new node.
+    ///
+    /// Returns the ID of the new node.
+    pub fn allocate_block(&mut self) -> Result<u64> {
+        // Make sure we still have enough space left
+        let new_offset = self.free_space_offset + NODE_BLOCK_ALIGNED_SIZE;
+        self.grow(new_offset)?;
+
+        // Return the old start of free space as block index
+        let result: u64 = (self.free_space_offset / NODE_BLOCK_ALIGNED_SIZE).try_into()?;
+
+        // Initialize some of the values
+        self.get_mut(result)?.num_keys_mut().write(0);
+        self.get_mut(result)?.is_leaf_mut().write(1);
+
+        // The next free block can be added after this block
+        self.free_space_offset = new_offset;
+        Ok(result)
+    }
+
+    /// Grows the file to contain at least the requested number of bytes.
+    /// This needs to copy all content into a new temporary file.
+    /// To avoid this costly operation, the file size is at least doubled.
+    fn grow(&mut self, requested_size: usize) -> Result<()> {
+        if requested_size <= self.mmap.len() {
+            // Still enough space, no action required
+            return Ok(());
+        }
+
+        // Create a new anonymous memory mapped the content is copied to.
+        // Allocate at least twice the old file size so we don't need to grow too often
+        let new_size = requested_size.max(self.mmap.len() * 2);
+        let mut new_mmap = MmapOptions::new().stack().len(new_size).map_anon()?;
+
+        // Copy all content from the old file into the new file
+        new_mmap[0..self.mmap.len()].copy_from_slice(&self.mmap);
+
+        self.mmap = new_mmap;
+        Ok(())
+    }
 }
 
 /// Representation of a header at the start of each block.
@@ -268,95 +392,4 @@ where
 }
 
 #[cfg(test)]
-mod tests {
-    use crate::file::BlockHeader;
-
-    use super::TemporaryBlockFile;
-
-    #[test]
-    fn grow_mmap_from_zero_capacity() {
-        // Create file with empty capacity
-        let mut m = TemporaryBlockFile::<u64>::with_capacity(0, 0).unwrap();
-        // The capacity must be at least one
-        assert_eq!(1, m.mmap.len());
-
-        // Needs to grow
-        m.grow(128).unwrap();
-        assert_eq!(128, m.mmap.len());
-        m.grow(4096).unwrap();
-        assert_eq!(4096, m.mmap.len());
-
-        // No growing necessar
-        m.grow(1024).unwrap();
-        assert_eq!(4096, m.mmap.len());
-
-        // Grow with double size
-        m.grow(8192).unwrap();
-        assert_eq!(8192, m.mmap.len());
-
-        // Grow with less than the double size still creates the double size
-        m.grow(9000).unwrap();
-        assert_eq!(16384, m.mmap.len());
-    }
-
-    #[test]
-    fn grow_mmap_with_capacity() {
-        let mut m = TemporaryBlockFile::<u64>::with_capacity(4096, 0).unwrap();
-        assert_eq!(4096, m.mmap.len());
-
-        // Don't grow if not necessary
-        m.grow(128).unwrap();
-        assert_eq!(4096, m.mmap.len());
-        m.grow(4096).unwrap();
-        assert_eq!(4096, m.mmap.len());
-
-        // Grow with double size
-        m.grow(8192).unwrap();
-        assert_eq!(8192, m.mmap.len());
-
-        // Grow with less than the double size still creates the double size
-        m.grow(9000).unwrap();
-        assert_eq!(16384, m.mmap.len());
-    }
-
-    #[test]
-    fn block_insert_get_update() {
-        let mut m = TemporaryBlockFile::<Vec<u64>>::with_capacity(128, 0).unwrap();
-        assert_eq!(128, m.mmap.len());
-
-        let mut b: Vec<u64> = std::iter::repeat(42).take(10).collect();
-        let idx = m.allocate_block(256 - BlockHeader::size()).unwrap();
-        // The block needs space for the data, but also for the header
-        assert_eq!(256, m.mmap.len());
-
-        // Insert the block as it is
-        assert_eq!(true, m.can_update(idx, &b).is_ok());
-        m.put(idx, &b).unwrap();
-
-        // Get and check it is still equal
-        let retrieved_block = m.get_owned(idx).unwrap();
-        assert_eq!(b, retrieved_block);
-
-        // The block should be able to hold a little bit more vector elements
-        for i in 1..20 {
-            b.push(i);
-        }
-        assert_eq!(true, m.can_update(idx, &b).is_ok());
-        m.put(idx, &b).unwrap();
-        let retrieved_block = m.get_owned(idx).unwrap();
-        assert_eq!(b, retrieved_block);
-
-        // We can't grow the block beyond the allocated limit
-        let mut large_block = b.clone();
-        for i in 1..300 {
-            large_block.push(i);
-        }
-        assert_eq!(false, m.can_update(idx, &large_block).unwrap().0);
-        // Check that we can still insert the block, but that the relocation table is updated
-        m.put(idx, &large_block).unwrap();
-        assert_eq!(1, m.relocated_blocks.len());
-        assert_eq!(true, m.relocated_blocks.contains_key(&idx));
-        // Get the block and check the new value is returned
-        assert_eq!(large_block, m.get_owned(idx).unwrap());
-    }
-}
+mod tests;
